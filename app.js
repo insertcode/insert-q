fdfc58bee55bd956106e6b804a96372ff6babe00
Fernando Larrañaga <xabadu@gmail.com>
2021-04-19 19:40:43 -0700

    Add prettier eslint plugin

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
-const fastify = require('fastify')({
-  logger: true
-})
+const fastify = require("fastify")({
+  logger: true,
+});
 
-fastify.get('/', async (request, reply) => {
-  return { hello: 'world' }
-})
+fastify.get("/", async () => ({ hello: "world" }));
 
 const start = async () => {
   try {
-    await fastify.listen(5000)
+    await fastify.listen(5000);
   } catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
+    fastify.log.error(err);
+    process.exit(1);
   }
-}
-start()
+};
+start();
