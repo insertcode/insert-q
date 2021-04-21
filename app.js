6dde91031639e3d9e48f8282650f65d6ffa2353e
Fernando Larrañaga <xabadu@gmail.com>
2021-04-20 20:36:04 -0700

    Add initial db schema and migrations (#4)

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const fastify = require("fastify")({
   logger: true,
 });
 
+require('dotenv').config()
+
+const knex = require('knex')({
+  client: 'mysql2',
+  connection: {
+    host : process.env.DB_HOST,
+    user : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_DB
+  }
+});
+
 fastify.get("/", async () => ({ hello: "world" }));
 
 const start = async () => {
